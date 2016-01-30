108a1e056c797fc27d6d7482d31515ce385ca5e9
steve simpson <stevesimpson4375@gmail.com>
2016-01-30 11:02:05 -0500

    Created initial classes

---
+++ PokerTournament/PokerTournament/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerTournament
+{
+    class Person : IComparable
+    {
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
